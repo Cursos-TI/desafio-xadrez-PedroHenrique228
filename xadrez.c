f672043a154a2fadca5613d5e6e9e795bac19bc3
PedroHenrique228 <pedrohrsilva280@gmail.com>
2025-05-06 22:37:04 +0000

    feat: adicionado movimentação do cavalo

--- xadrez.c
+++ xadrez.c
@@ -7,6 +7,7 @@ int main() {
     int numeroDeCasasTorre = 5;
     int numeroDeCasasBispo = 5;
     int numeroDeCasasDama = 8;
+    int numeroDeCasasCavalo = 1;
 
     do{
         //Selecionar o movimento
@@ -14,11 +15,12 @@ int main() {
         printf("1 - Torre\n");
         printf("2 - Bispo\n");
         printf("3 - Dama\n");
-        printf("4 - Sair\n");
+        printf("4 - Cavalo\n");
+        printf("5 - Sair\n");
         scanf("%d", &opcao);
 
         
-        if(opcao == 1){ //Movimento da torre
+        if(opcao == 1){                             //Movimento da torre
             while(numeroDeCasasTorre > 0){
                 printf("Direita\n");
                 numeroDeCasasTorre--;
@@ -26,7 +28,7 @@ int main() {
             //Reiniciando contagem de casas da torre
             int numeroDeCasasTorre = 5;
 
-        }else if(opcao == 2){ //Movimento do bispo
+        }else if(opcao == 2){                      //Movimento do bispo
             do
             {
                 printf("Cima\n");
@@ -36,15 +38,24 @@ int main() {
             //Reiniciando contagem de casas do bispo
             int numeroDeCasasBispo = 5;
             
-        }else if(opcao == 3){ //Movimento da rainha
+        }else if(opcao == 3){                     //Movimento da rainha
             for(int i = 1; i <= numeroDeCasasDama; i++){
                 printf("Esquerda\n");
             }
             //Reiniciando contagem de casas da dama
             int numeroDeCasasDama = 8;
+        }else if(opcao == 4){                     //Movimento do cavalo
+            for(int i = 1; i <= numeroDeCasasCavalo; i++){
+                for(int j = 1; j <= numeroDeCasasCavalo*2; j++){
+                    printf("Baixo\n");
+                }
+                printf("Esquerda\n");
+            }
+            //Reiniciando contagem de casas do cavalo
+            int numeroDeCasasCavalo = 1;
         }
 
-    }while(opcao != 4);
+    }while(opcao != 5);
 
     return 0;
 }
