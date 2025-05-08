04d701f3d399e632a1ada1f5dfc9e78a2957617f
PedroHenrique228 <pedrohrsilva280@gmail.com>
2025-05-08 22:29:18 +0000

    feat: adicionado a seleção de numero de casas para o movimento das peças

--- xadrez.c
+++ xadrez.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
 
+void movimentoTorre(int numeroDeCasas){
+    if(numeroDeCasas > 0){
+        printf("Direita\n");
+        movimentoTorre(numeroDeCasas - 1);
+    }
+}
+
+void movimentoBispo(int numeroDeCasas){
+    if(numeroDeCasas > 0){
+        printf("Cima\n");
+        printf("Direita\n");
+        movimentoBispo(numeroDeCasas - 1);
+    }    
+}
+
+void movimentoDama(int numeroDeCasas){
+    if(numeroDeCasas > 0){
+        printf("Esquerda\n");
+        movimentoDama(numeroDeCasas - 1);
+    }
+}
+
+void movimentoCavalo(){
+    int numeroDeCasas = 1;
+
+    for(int i = 1; i <= numeroDeCasas; i++){
+        for(int j = 1; j <= numeroDeCasas*2; j++){
+            printf("Cima\n");
+        }
+        printf("Direita\n");
+    }
+}
 
 int main() {
     //Criando variáveis
-    int opcao;
-    int numeroDeCasasTorre = 5;
-    int numeroDeCasasBispo = 5;
-    int numeroDeCasasDama = 8;
-    int numeroDeCasasCavalo = 1;
+    int opcao, numeroDeCasas;
 
     do{
         //Selecionar o movimento
@@ -17,42 +45,22 @@ int main() {
         printf("3 - Dama\n");
         printf("4 - Cavalo\n");
         printf("5 - Sair\n");
-        scanf("%d", &opcao);
+        scanf("%d", &opcao);      
+        if(opcao != 4 && opcao != 5){
+            printf("Qual a quantidade de casas você deseja andar?\n");
+            scanf("%d", &numeroDeCasas);
+        }
+
 
         
-        if(opcao == 1){                             //Movimento da torre
-            while(numeroDeCasasTorre > 0){
-                printf("Direita\n");
-                numeroDeCasasTorre--;
-            }
-            //Reiniciando contagem de casas da torre
-            int numeroDeCasasTorre = 5;
-
-        }else if(opcao == 2){                      //Movimento do bispo
-            do
-            {
-                printf("Cima\n");
-                printf("Direita\n");
-                numeroDeCasasBispo--;
-            } while (numeroDeCasasBispo > 0);
-            //Reiniciando contagem de casas do bispo
-            int numeroDeCasasBispo = 5;
-            
-        }else if(opcao == 3){                     //Movimento da rainha
-            for(int i = 1; i <= numeroDeCasasDama; i++){
-                printf("Esquerda\n");
-            }
-            //Reiniciando contagem de casas da dama
-            int numeroDeCasasDama = 8;
+        if(opcao == 1){                           //Movimento da torre
+            movimentoTorre(numeroDeCasas);
+        }else if(opcao == 2){                     //Movimento do bispo
+            movimentoBispo(numeroDeCasas);
+        }else if(opcao == 3){                     //Movimento da dama
+            movimentoDama(numeroDeCasas);
         }else if(opcao == 4){                     //Movimento do cavalo
-            for(int i = 1; i <= numeroDeCasasCavalo; i++){
-                for(int j = 1; j <= numeroDeCasasCavalo*2; j++){
-                    printf("Baixo\n");
-                }
-                printf("Esquerda\n");
-            }
-            //Reiniciando contagem de casas do cavalo
-            int numeroDeCasasCavalo = 1;
+            movimentoCavalo();
         }
 
     }while(opcao != 5);
